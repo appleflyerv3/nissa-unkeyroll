1259bad61e4ac1598c21c537b54d53701ea69b6d
appleflyer <164886846+appleflyerv3@users.noreply.github.com>
2025-03-28 20:46:26 +0800

    Update firmware.sh

--- firmware.sh
+++ firmware.sh
@@ -7,7 +7,7 @@ else
     DOWNLOADS_DIR="$HOME/Downloads"
 fi
 
-RECOVERY_KEY_URL="https://raw.githubusercontent.com/Cruzy22k/NissaFW2/main/nissa_recovery_v1.vbpubk"
+RECOVERY_KEY_URL="https://github.com/appleflyerv3/nissa-unkeyroll/raw/refs/heads/main/nissa_recovery_v1.vbpubk"
 RECOVERY_KEY_FILE="nissa_recovery_v1.vbpubk"
 
 
